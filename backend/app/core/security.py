2e97157b693c27f9cda86b044878b65eccf28b20
Ben Hancock <hancocb@purdue.edu>
2023-06-18 11:00:05 -0400

    add password hashing with passlib and bcrypt, add method for generating JWT tokens, add type stubs for jose and passlib to poetry dev group

---
+++ backend/app/core/security.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from typing import Any
+
+from jose import jwt
+from passlib.context import CryptContext
+
+from app.core.config import settings
+
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",  # only allow bcrypt
+)
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(subject: str | Any) -> str:
+    """Create JWT access token with expiration defined in settings."""
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Compare plain text password against hashed password and return true if match."""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """Use bcrypt to create hashed password."""
+    return pwd_context.hash(password)
--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from sqlalchemy.orm import Session
 
-# from app.core.security import get_password_hash, verify_password
+from app.core.security import get_password_hash, verify_password
 from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
@@ -15,8 +15,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
         db_obj = User(
             email=obj_in.email,
-            # hashed_password=get_password_hash(obj_in.password),
-            hashed_password=f"hash-{obj_in.password}",
+            hashed_password=get_password_hash(obj_in.password),
             full_name=obj_in.full_name,
         )
         db.add(db_obj)
@@ -32,8 +31,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         else:
             update_data = obj_in.dict(exclude_unset=True)
         if update_data["password"]:
-            # hashed_password = get_password_hash(update_data["password"])
-            hashed_password = f"hash-{update_data['password']}"
+            hashed_password = get_password_hash(update_data["password"])
             del update_data["password"]
             update_data["hashed_password"] = hashed_password
         return super().update(db, db_obj=db_obj, obj_in=update_data)
@@ -42,16 +40,13 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         user = self.get_by_email(db, email=email)
         if not user:
             return None
-        # if not verify_password(password, user.hashed_password):
-        #     return None
+        if not verify_password(password, user.hashed_password):
+            return None
         return user
 
     def is_active(self, user: User) -> bool:
         # returns false if user.is_active is None or False
-        if user.is_active:
-            return True
-        else:
-            return False
+        return user.is_active
 
     def is_superuser(self, user: User) -> bool:
         return user.is_superuser
