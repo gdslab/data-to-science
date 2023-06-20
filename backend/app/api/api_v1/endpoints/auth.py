69bd5c96350d216a7762d5d9f9545e8d560fe3c8
Ben Hancock <hancocb@purdue.edu>
2023-06-20 13:19:13 -0400

    fully implement oauth2 with password and jwt tokens

--- backend/app/api/api_v1/endpoints/auth.py
+++ backend/app/api/api_v1/endpoints/auth.py
@@ -1,16 +1,38 @@
-from fastapi import APIRouter, status
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+from app.core import security
+from app.core.config import settings
+
 
 router = APIRouter()
 
 
-@router.post("/login/")
-async def login():
-    pass
+@router.post("/access-token", response_model=schemas.Token)
+def login_access_token(
+    db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
+) -> Any:
+    """OAuth2 compatible token login, get an access token for future requests."""
+    print(form_data)
+    user = crud.user.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid credentials")
+    elif not crud.user.is_active(user):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive account")
+    return {
+        "access_token": security.create_access_token(user.id),
+        "token_type": "bearer"
+    }
 
-@router.post("/logout/", status_code=status.HTTP_204_NO_CONTENT)
-async def logout():
-    pass
 
-@router.post("/changepassword/")
-async def change_password():
-    pass
+@router.post("/test-token", response_model=schemas.User)
+def test_token(current_user: models.User = Depends(deps.get_current_user)) -> Any:
+    """Test access token."""
+    return current_user
--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,4 +1,20 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from jose.exceptions import JWTError
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.core import security
+from app.core.config import settings
 from app.db.session import SessionLocal
+
+
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/auth/access-token"
+)
 
 
 def get_db():
@@ -8,3 +24,20 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)) -> models.User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = schemas.TokenPayload(**payload)
+    except (JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials"
+        )
+    user = crud.user.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
