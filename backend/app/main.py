3b839ac9f86dbe40885674357b9976fea7fba9f5
Ben Hancock <hancocb@purdue.edu>
2023-07-03 15:32:25 -0400

    drop cors middleware from backend (for now)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 import logging
 
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
@@ -10,21 +9,6 @@ from app.core.logging import setup_logger
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
-)
-
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://frontend",
-    "http://frontend:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 setup_logger()
