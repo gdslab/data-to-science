a4b08ef085f67de4a4c797f38ed97e2516d133ee
Ben Hancock <hancocb@purdue.edu>
2024-01-09 11:01:56 -0500

    add divider to navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -72,82 +72,89 @@ export default function Navbar() {
                 ) : null}
               </div>
               {user ? (
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3 text-white">
-                    <Menu.Button className="relative flex rounded-full">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <div className="flex items-center justify-center">
-                        {user.profile_url ? (
-                          <img
-                            key={user.profile_url.split('/').slice(-1)[0].slice(0, -4)}
-                            className="h-8 w-8 rounded-full"
-                            src={user.profile_url}
-                          />
-                        ) : (
-                          <div
-                            className="flex items-center justify-center h-8 w-8 text-sm rounded-full"
-                            style={generateRandomProfileColor(
-                              user.first_name + ' ' + user.last_name
-                            )}
-                          >
-                            {`${user.first_name[0]} ${user.last_name[0]}`}
-                          </div>
-                        )}
+                <div className="h-full min-w-56 flex items-center justify-center border-l-2 border-white">
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-3 text-white">
+                      <Menu.Button className="relative flex rounded-full">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <div className="flex items-center justify-center">
+                          {user.profile_url ? (
+                            <img
+                              key={user.profile_url
+                                .split('/')
+                                .slice(-1)[0]
+                                .slice(0, -4)}
+                              className="h-8 w-8 rounded-full"
+                              src={user.profile_url}
+                            />
+                          ) : (
+                            <div
+                              className="flex items-center justify-center h-8 w-8 text-sm rounded-full"
+                              style={generateRandomProfileColor(
+                                user.first_name + ' ' + user.last_name
+                              )}
+                            >
+                              {`${user.first_name[0]} ${user.last_name[0]}`}
+                            </div>
+                          )}
 
-                        <div className="hidden md:inline ml-4">
-                          {user.first_name} {user.last_name}
+                          <div className="hidden md:inline ml-4">
+                            {user.first_name} {user.last_name}
+                          </div>
+                          <ChevronDownIcon
+                            className="inline h-4 w-4 ml-2"
+                            aria-hidden="true"
+                          />
                         </div>
-                        <ChevronDownIcon
-                          className="inline h-4 w-4 ml-2"
-                          aria-hidden="true"
-                        />
-                      </div>
-                    </Menu.Button>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              to="/auth/profile"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700 visited:text-gray-700'
-                              )}
-                            >
-                              Your Profile
-                            </Link>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              to="/auth/logout"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700 visited:text-gray-700'
-                              )}
-                            >
-                              Sign out
-                            </Link>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
+                      </Menu.Button>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link
+                                to="/auth/profile"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700 visited:text-gray-700'
+                                )}
+                              >
+                                Your Profile
+                              </Link>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link
+                                to="/auth/logout"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700 visited:text-gray-700'
+                                )}
+                              >
+                                Sign out
+                              </Link>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </div>
                 </div>
               ) : (
-                <div className="absolute inset-y-0 right-0 flex items-center text-white pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  <Link to="/auth/login">Sign in</Link>
+                <div className="h-full min-w-56 flex items-center justify-center border-l-2 border-white">
+                  <div className="absolute inset-y-0 right-0 flex items-center text-white pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <Link to="/auth/login">Sign in</Link>
+                  </div>
                 </div>
               )}
             </div>
