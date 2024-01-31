29f38ceaacf052b8fcc92e84d5ab23275d24ee00
Ben Hancock <hancocb@purdue.edu>
2024-01-30 20:56:33 -0500

    only close upload modal when "done" button clicked or pressed

--- frontend/src/components/UploadModal.tsx
+++ frontend/src/components/UploadModal.tsx
@@ -142,12 +142,12 @@ export default function UploadModal({ apiRoute, open, setOpen, uploadType }: Pro
   }
 
   return (
-    <Transition.Root show={open} as={Fragment}>
+    <Transition.Root appear show={open} as={Fragment}>
       <Dialog
         as="div"
         className="relative z-10"
         initialFocus={cancelButtonRef}
-        onClose={setOpen}
+        onClose={() => null}
       >
         <Transition.Child
           as={Fragment}
@@ -197,12 +197,16 @@ export default function UploadModal({ apiRoute, open, setOpen, uploadType }: Pro
                   <DtypeRadioInput dtype={dtype} setDtype={setDtype} />
                 ) : null}
 
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="flex items-center justify-between bg-gray-50 px-4 py-3 sm:px-6">
+                  <span>
+                    Click <strong>Done</strong> when you have finished uploading files.
+                  </span>
                   <button
+                    ref={cancelButtonRef}
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    aria-label="Done button"
                     onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
                   >
                     Done
                   </button>
