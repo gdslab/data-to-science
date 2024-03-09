05f19f30b7023a573907253218cb8751bcc62525
Ben Hancock <hancocb@purdue.edu>
2024-03-09 10:50:32 -0500

    adjust validation rules for planting and harvest dates

--- frontend/src/components/pages/projects/flights/validationSchema.tsx
+++ frontend/src/components/pages/projects/flights/validationSchema.tsx
@@ -3,8 +3,14 @@ import * as Yup from 'yup';
 const validationSchema = Yup.object({
   acquisitionDate: Yup.date().required('Required'),
   altitude: Yup.number().positive('Must be greater than 0').required('Required'),
-  sideOverlap: Yup.number().positive('Must be greater than 0').required('Required'),
-  forwardOverlap: Yup.number().positive('Must be greater than 0').required('Required'),
+  sideOverlap: Yup.number()
+    .min(0, 'Cannot be lower than 0%')
+    .max(100, 'Cannot be higher than 100%')
+    .required('Required'),
+  forwardOverlap: Yup.number()
+    .min(0, 'Cannot be lower than 0%')
+    .max(100, 'Cannot be higher than 100%')
+    .required('Required'),
   sensor: Yup.string().required('Required'),
   platform: Yup.string().required('Required'),
   platformOther: Yup.string().when('platform', {
