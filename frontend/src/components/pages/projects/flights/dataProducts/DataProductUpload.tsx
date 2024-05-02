efdb5aedff6239921ab21104415cb4f403a670ca
Ben Hancock <hancocb@purdue.edu>
2024-05-01 21:31:26 -0400

    include progress details for data product uploads

--- frontend/src/components/pages/projects/flights/dataProducts/DataProductUpload.tsx
+++ frontend/src/components/pages/projects/flights/dataProducts/DataProductUpload.tsx
@@ -115,6 +115,7 @@ export default function DataProductUpload({
       }}
       proudlyDisplayPoweredByUppy={false}
       disableThumbnailGenerator={uploadType !== 'img'}
+      showProgressDetails={true}
     />
   );
 }
