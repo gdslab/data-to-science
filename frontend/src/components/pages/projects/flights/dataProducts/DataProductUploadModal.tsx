b2a36e0b17233f196b691644ef0a2171e12a14e7
Ben Hancock <hancocb@purdue.edu>
2024-05-05 16:40:34 -0400

    store "other" data type value

--- frontend/src/components/pages/projects/flights/dataProducts/DataProductUploadModal.tsx
+++ frontend/src/components/pages/projects/flights/dataProducts/DataProductUploadModal.tsx
@@ -105,7 +105,7 @@ export default function DataProductUploadModal({
                   <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                     <DataProductUpload
                       info={{
-                        dtype: dtype,
+                        dtype: dtype === 'other' ? dtypeOther : dtype,
                         endpoint: '/files',
                         flightID: flightID,
                         projectID: projectID,
