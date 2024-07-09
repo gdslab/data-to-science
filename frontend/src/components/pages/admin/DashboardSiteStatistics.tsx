e096b27e862cfcaea1570777344fa870f33a72ae
Ben Hancock <hancocb@purdue.edu>
2024-07-09 09:23:37 -0400

    better handle wait for site statistics

--- frontend/src/components/pages/admin/DashboardSiteStatistics.tsx
+++ frontend/src/components/pages/admin/DashboardSiteStatistics.tsx
@@ -1,23 +1,43 @@
 import axios, { AxiosResponse } from 'axios';
-import { useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Await, defer, useLoaderData } from 'react-router-dom';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
 import StatCards from './StatCards';
 
 import { SiteStatistics } from './DashboardTypes';
+import StatCardsSkeleton from './StatCardsSkeleton';
 
 export async function loader() {
-  const response: AxiosResponse<SiteStatistics> = await axios.get(
+  const response: Promise<AxiosResponse<SiteStatistics>> = axios.get(
     `${import.meta.env.VITE_API_V1_STR}/admin/site_statistics`
   );
   if (response) {
-    return response.data;
+    return defer({ stats: response });
   } else {
     return [];
   }
 }
 
-export default function DashboardSiteStatistics() {
-  const stats = useLoaderData() as SiteStatistics;
+function ErrorElement() {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <ExclamationTriangleIcon className="h-10 w-10 text-red-600" />
+      <span className="text-2xl font-semibold text-primary">
+        Error occurred. Unable to load statistics.
+      </span>
+    </div>
+  );
+}
 
-  return <StatCards stats={stats} />;
+export default function DashboardSiteStatistics() {
+  const data = useLoaderData() as { stats: Promise<SiteStatistics> };
+
+  return (
+    <Suspense fallback={<StatCardsSkeleton />}>
+      <Await resolve={data.stats} errorElement={<ErrorElement />}>
+        {(stats) => <StatCards stats={stats.data} />}
+      </Await>
+    </Suspense>
+  );
 }
