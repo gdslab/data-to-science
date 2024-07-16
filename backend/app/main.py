027ae27745a3ffbc3bd33bfd47b484ae3e5de3de
Ben Hancock <hancocb@purdue.edu>
2024-07-15 20:59:16 -0400

    move titiler from backend container to its own container

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,6 @@ from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from starlette.background import BackgroundTask
-from titiler.core.factory import TilerFactory
-from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
@@ -22,18 +20,13 @@ app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_methods=["GET"], allow_headers=["*"]
 )
 
-cog = TilerFactory()
-
 setup_logger()
 logger = logging.getLogger(__name__)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-app.include_router(cog.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
 
 app.mount("/static", ProtectedStaticFiles(directory=settings.STATIC_DIR), name="static")
 app.mount("/potree", StaticFiles(directory=settings.POTREE_DIR), name="potree")
-
-add_exception_handlers(app, DEFAULT_STATUS_CODES)
 
 
 def write_access_log(request: Request, response: Response, process_time: float):
