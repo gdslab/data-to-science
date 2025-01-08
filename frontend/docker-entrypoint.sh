ca1e049dfcb6cb4651eebb5017e4ef49b771b8d7
Ben Hancock <hancocb@purdue.edu>
2025-01-08 13:12:49 -0500

    overwrite existing frontend config.json each time entrypoint runs

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Generate a runtime configuration file with the environment variable
-echo "{\"mapboxAccessToken\": \"${VITE_MAPBOX_ACCESS_TOKEN}\"}" >> /usr/share/nginx/html/config.json
+echo "{\"mapboxAccessToken\": \"${VITE_MAPBOX_ACCESS_TOKEN}\"}" > /usr/share/nginx/html/config.json
 
 exec "$@"
