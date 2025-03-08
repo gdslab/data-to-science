cec40e2fc3fb3cc6aa31a642dae2658dc53e0f27
Ben Hancock <hancocb@purdue.edu>
2025-03-08 14:25:01 -0500

    update to d2s logo

--- frontend/src/components/pages/Welcome.tsx
+++ frontend/src/components/pages/Welcome.tsx
@@ -1,10 +1,14 @@
-import brandLogo from '../../assets/d2s-logo-black.png';
+import brandLogo from '../../assets/d2s-logo-blue.svg';
 
 export default function Welcome({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col gap-2 items-center jusitfy-center m-4">
       <div className="flex items-center justify-center">
-        <img className="h-16 w-16" src={brandLogo} alt="Brand Logo" />
+        <img
+          className="max-h-12 max-w-full h-auto object-contain"
+          src={brandLogo}
+          alt="Brand Logo"
+        />
       </div>
       <div className="text-center">
         <h1 className="text-black mb-0">Welcome!</h1>
