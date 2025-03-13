c11aa888df6504d6ef30af6409b475dc040bbbb8
Ben Hancock <hancocb@purdue.edu>
2025-03-13 11:46:29 -0400

    set number of celery workers from environment settings

--- backend/celery/worker-start.sh
+++ backend/celery/worker-start.sh
@@ -5,4 +5,7 @@ set -o nounset
 
 python /app/app/celeryworker_pre_start.py
 
-celery -A app.core.celery_app worker -l info -c 4
+# Default to 4 workers if not set
+CELERY_CONCURRENCY=${CELERY_WORKERS:-4}
+
+celery -A app.core.celery_app worker -l info -c "${CELERY_CONCURRENCY}"
