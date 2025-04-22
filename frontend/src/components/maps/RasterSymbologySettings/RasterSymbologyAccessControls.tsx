939df9bc0d52297271619aabe2d194239f623dc0
hancocb <hancocb@purdue.edu>
2025-04-22 15:49:31 -0400

    remove unused import

--- frontend/src/components/maps/RasterSymbologySettings/RasterSymbologyAccessControls.tsx
+++ frontend/src/components/maps/RasterSymbologySettings/RasterSymbologyAccessControls.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ChevronDownIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 import Alert, { Status } from '../../Alert';
 import {
