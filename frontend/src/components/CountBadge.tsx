cd821dd4f72834c96a920c9986ac4a077b7b618b
hancocb <hancocb@purdue.edu>
2025-05-05 22:54:27 -0400

    update CountBadge.tsx

--- frontend/src/components/CountBadge.tsx
+++ frontend/src/components/CountBadge.tsx
@@ -18,10 +18,15 @@ export default function CountBadge({
       className={clsx(
         'inline-flex items-center justify-center rounded-full px-2.5 py-0.5',
         {
-          [`text-${color}-700`]: true,
-          [`bg-${color}-50`]: count === 0,
-          [`bg-${color}-100`]: count > 0 && count < 5,
-          [`bg-${color}-200`]: count > 4,
+          // Green color scheme
+          'text-green-700 bg-green-50': color === 'green' && count === 0,
+          'text-green-700 bg-green-100':
+            color === 'green' && count > 0 && count < 5,
+          'text-green-700 bg-green-200': color === 'green' && count > 4,
+          // Sky color scheme
+          'text-sky-700 bg-sky-50': color === 'sky' && count === 0,
+          'text-sky-700 bg-sky-100': color === 'sky' && count > 0 && count < 5,
+          'text-sky-700 bg-sky-200': color === 'sky' && count > 4,
         }
       )}
     >
