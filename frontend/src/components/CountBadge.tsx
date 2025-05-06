821d606fb118dcdea1cc618aeb0012e80e39338c
Ben Hancock <hancocb@purdue.edu>
2025-05-06 09:27:02 -0400

    adds options to sort by flight and data product counts

--- frontend/src/components/CountBadge.tsx
+++ frontend/src/components/CountBadge.tsx
@@ -5,6 +5,7 @@ interface CountBadgeProps {
   color: 'green' | 'sky';
   label: string;
   icon?: React.ReactNode;
+  rank: 'low' | 'medium' | 'high';
 }
 
 export default function CountBadge({
@@ -12,21 +13,24 @@ export default function CountBadge({
   color,
   label,
   icon,
+  rank,
 }: CountBadgeProps) {
+  console.log('count', count);
+  console.log('color', color);
+  console.log('rank', rank);
   return (
     <span
       className={clsx(
         'inline-flex items-center justify-center rounded-full px-2.5 py-0.5',
         {
           // Green color scheme
-          'text-green-700 bg-green-50': color === 'green' && count === 0,
-          'text-green-700 bg-green-100':
-            color === 'green' && count > 0 && count < 5,
-          'text-green-700 bg-green-200': color === 'green' && count > 4,
+          'text-green-700 bg-green-50': color === 'green' && rank === 'low',
+          'text-green-700 bg-green-100': color === 'green' && rank === 'medium',
+          'text-green-700 bg-green-200': color === 'green' && rank === 'high',
           // Sky color scheme
-          'text-sky-700 bg-sky-50': color === 'sky' && count === 0,
-          'text-sky-700 bg-sky-100': color === 'sky' && count > 0 && count < 5,
-          'text-sky-700 bg-sky-200': color === 'sky' && count > 4,
+          'text-sky-700 bg-sky-50': color === 'sky' && rank === 'low',
+          'text-sky-700 bg-sky-100': color === 'sky' && rank === 'medium',
+          'text-sky-700 bg-sky-200': color === 'sky' && rank === 'high',
         }
       )}
     >
