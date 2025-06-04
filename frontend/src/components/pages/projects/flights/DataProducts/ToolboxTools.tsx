2e721a5ce366884caf463b7286d9368b88fffcfd
hancocb <hancocb@purdue.edu>
2025-06-04 14:57:55 -0400

    drop data product id parameter

--- frontend/src/components/pages/projects/flights/DataProducts/ToolboxTools.tsx
+++ frontend/src/components/pages/projects/flights/DataProducts/ToolboxTools.tsx
@@ -74,7 +74,7 @@ const VARIBandSelection = ({ dataProduct }: { dataProduct: DataProduct }) => {
   );
 };
 
-const HillshadeTools = ({ dataProductId }: { dataProductId: string }) => {
+const HillshadeTools = () => {
   return (
     <ul>
       <li>
