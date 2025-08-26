c610af244d617170a04e1711972accb16487e130
Ben Hancock <hancocb@purdue.edu>
2025-08-26 11:34:49 -0400

    drop unnecessary cors changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,18 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
     hmr: {
       path: '/hmr',
-    },
-  },
-  preview: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
 });
